05549b3a2e1ecd20a6e50c5c31cc8a11f2e5d781
Jason Eastman <jasont41@gmail.com>
2020-09-20 10:42:10 -0700

    4 way movement working

--- 4400 game/Assets/Scripts/PlayerMovement.cs
+++ 4400 game/Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,36 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public float speed;
     private Rigidbody2D myRigidBody;
-    private Vector3 change; 
+    private Vector3 change;
+    private Animator animator; 
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if(change != Vector3.zero)
+        UpdateAnimationAndMove(); 
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
         {
-            MoveCharacter(); 
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
         }
     }
     void MoveCharacter()
