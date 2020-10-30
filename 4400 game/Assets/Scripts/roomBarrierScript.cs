3720b39ae0d309f2e8269c7e6e545823daf8b59c
jasont41 <43120995+jasont41@users.noreply.github.com>
2020-10-30 01:07:53 -0700

    Added room barriers that will be used as level requirements

---
+++ 4400 game/Assets/Scripts/roomBarrierScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI; 
+
+
+public class roomBarrierScript : MonoBehaviour
+{
+    public GameObject textBox;
+    public Text text;
+
+
+    private void Awake()
+    {
+    }
+
+    public string warningString; 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        textBox = canvas_dont_destroy.Instance.dialogBox;
+        text = canvas_dont_destroy.Instance.dialogBox_text; 
+        if (collision.CompareTag("Player"))
+        {
+            text.text = warningString; 
+            textBox.SetActive(true); 
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textBox.SetActive(false);
+        }
+    }
+    IEnumerator printingMsg()
+    {
+        yield return new WaitForSeconds(1f); 
+    }
+}
