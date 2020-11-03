4f2451606c13f1b5ed81df7e1f79420e8ac22dcf
jasont41 <43120995+jasont41@users.noreply.github.com>
2020-11-02 17:37:28 -0800

    more inventory

---
+++ 4400 game/Assets/Scripts/Items/HealthPotion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : Items
+{
+    public int heal_val = 20; 
+    // Start is called before the first frame update
+    public override void useItem()
+    {
+        PlayerMovement.Instance.addHealth(heal_val); 
+    }
+
+}
---
+++ 4400 game/Assets/Scripts/Items/Items.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items 
+{
+    public virtual void useItem()
+    {
+        //to be overloaded 
+    }
+}
