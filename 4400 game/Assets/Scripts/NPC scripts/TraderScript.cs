5132394901266f660cb8a32a313e238e63ed04a7
Jason Eastman <jasont41@gmail.com>
2020-12-10 05:56:27 -0800

    Arrows added, more work on trader

--- 4400 game/Assets/Scripts/NPC scripts/TraderScript.cs
+++ 4400 game/Assets/Scripts/NPC scripts/TraderScript.cs
@@ -7,13 +7,25 @@ public class TraderScript : MonoBehaviour
 {
     public bool playerInRange;
     public GameObject TraderUI;
-
+    public int potionQuan;
+    public int potionTotal;
+    public int cartTotal;
+    public int potionCost;
+    public Text potionCostText;
+    public Text potionQuanText; 
     private void Start()
     {
+        potionQuan = potionTotal = cartTotal = 0; //zero it all out 
         TraderUI = canvas_dont_destroy.Instance.traderUI;
+        potionCost = 50;
+        
     }
     private void Update()
     {
+        if (TraderUI.activeInHierarchy == true)
+        {
+             
+        }
         if(Input.GetKeyDown(KeyCode.Space) && playerInRange)
         {
             Debug.Log("Got here - Trader"); 
@@ -25,6 +37,11 @@ public class TraderScript : MonoBehaviour
             else
             {
                 TraderUI.SetActive(true);
+                potionCostText = GameObject.FindGameObjectWithTag("PotionCostText").GetComponent<Text>();
+                potionCostText = GameObject.FindGameObjectWithTag("PotionQuantity").GetComponent<Text>();
+                potionQuanText.text = potionQuan.ToString(); 
+                potionCostText.text = "Cost: $" + potionCost.ToString();
+                
                 PlayerMovement.Instance.prevent_movement = false; 
             }
         }
